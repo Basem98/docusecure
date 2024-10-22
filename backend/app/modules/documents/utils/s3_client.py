930f35b5fb4f587cf328e076bf63b7a7d7120c14
basem98 <basem.mostafa98@hotmail.com>
2024-10-22 21:15:04 +0300

    feat: Create a method in s3client to generate urls from file_path

--- backend/app/modules/documents/utils/s3_client.py
+++ backend/app/modules/documents/utils/s3_client.py
@@ -27,10 +27,16 @@ class S3Client(metaclass=Singleton):
             file_path = f'{folder_name}/{datetime.now().timestamp()
                                          }-{file_name}'
             self._client.upload_fileobj(file, self._bucket_name, file_path)
+            return file_path
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=500, detail="File upload failed")
+    
+    def generate_s3_url(self, file_path):
+        try:
             url = self._client.generate_presigned_url(
                 "get_object", {"Bucket": self._bucket_name, "Key": file_path}, ExpiresIn=3600)
-            print(url)
-            return file_path
+            return url
         except Exception as e:
             print(e)
             raise HTTPException(status_code=500, detail="File upload failed")
