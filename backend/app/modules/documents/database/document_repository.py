c23037b88b770600c948ce533482e96402c1ceb2
basem98 <basem.mostafa98@hotmail.com>
2024-10-23 19:04:06 +0300

    fix: Fix insertion problem by using kwargs instead of dict param

--- backend/app/modules/documents/database/document_repository.py
+++ backend/app/modules/documents/database/document_repository.py
@@ -3,5 +3,5 @@ class FileRepository:
         print(file_model)
         self._file_model = file_model
     async def create_file(self, fileData):
-        result = await self._file_model(fileData).insert()
+        result = await self._file_model(**fileData).insert()
         return result
