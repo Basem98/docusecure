a25b586d2d6b98049dfe8393504894ba2b6ca738
basem98 <basem.mostafa98@hotmail.com>
2024-10-23 20:25:37 +0300

    fix: Use user_id instead of static dummy filepath in s3

--- backend/app/modules/documents/services/documents_service.py
+++ backend/app/modules/documents/services/documents_service.py
@@ -9,14 +9,15 @@ class DocumentService:
         self._settings = settings
         
     async def upload_document(self, file: UploadFile):
-        result = await self._file_uploader.upload(file)
+        user_id = "123124"
+        result = await self._file_uploader.upload(user_id, file)
         file_data = {
             "file_path": result,
             "bucket_name": self._settings.AWS_S3_BUCKET_NAME,
             "file_format": re.findall("(?<=\.)[a-zA-Z]+", file.filename)[0],
             "content_type": file.content_type,
             "file_size": file.size,
-            "user_id": "123124",
+            "user_id": user_id,
             "date_created": datetime.now().timestamp(),
             "status": "uploaded",
             "classification": "classified"
--- backend/app/modules/documents/utils/file_uploader_util.py
+++ backend/app/modules/documents/utils/file_uploader_util.py
@@ -5,6 +5,6 @@ class FileUploader:
     def __init__(self, s3_client):
         self._s3_client = s3_client
 
-    async def upload(self, file: UploadFile):
-        file_path = await self._s3_client.upload("sdfvcwfeqw1234234234243", file.filename, file.file)
+    async def upload(self, user_id, file: UploadFile):
+        file_path = await self._s3_client.upload(user_id, file.filename, file.file)
         return file_path
